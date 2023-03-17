c075adf38772169ea6d51bad100092350fd7d3b0
knockjoy <87211271+Knockjoy@users.noreply.github.com>
2023-03-17 20:56:41 +0900

    Add files via upload

---
+++ arepl_helper.py
@@ -0,0 +1,59 @@
+##########################
+# -実行ファイルと同じ階層に
+#  input.txt,output.pyを配置
+##########################
+import inspect
+import re
+
+class HelperError(Exception):
+    pass
+
+class Input_reader:
+    def __init__(self):
+        self.data = self.reader("input.txt").splitlines()
+        self.ans_chacker = False
+        self.count=0
+
+    def compile(self):
+        frame = inspect.stack()[1]
+        filename = frame[1]
+        self.file_data = self.reader(filename)
+        const = re.search(r'.*(?=\s*=\s*Input_reader\(\)\s*)',
+                          self.file_data).group().strip()#変数定義を検索
+        self.file_data = re.sub(
+            r'from arepl_helper import \*', '', self.file_data)#import文を空白にする
+        self.file_data = re.sub(r'.*Input_reader\(\)', '', self.file_data)#変数定義を空白にする
+        self.file_data = re.sub(
+            rf'{const}.input\(\)', 'input()', self.file_data)#Input()を組み込み関数のinputにセットする
+        self.file_data = re.sub(rf'{const}.compile\(\)', '', self.file_data)#compile()を削除
+        self.file_data.strip()
+        with open("output.py", "w") as f:
+            f.truncate(0)
+            f.write(self.file_data)
+
+    def input(self)->str:
+        if len(self.data)!=self.count:
+            res= self.data[self.count]
+            self.count+=1
+            # if len(self.data)!=self.count:
+            #     print("inputできる要素がまだ残っています。")
+            return res
+        else :raise HelperError("inputする要素がありません。")
+
+    def new_file(self, num):
+        frame = inspect.stack()[1]
+        filename = frame[1]
+        with open(filename, "w") as f2:
+            file_data = f2.read()
+            f2.truncate(0)
+            f2.write('''
+                    from arepl_helper import *\n
+                    helper=Input_reader()\n
+                    #helper.compile()\n
+                        ''')
+        with open(f"py_list/{num}.py", "w") as f:
+            f.write(file_data)
+
+    def reader(self, path):
+        with open(path, "r") as f:
+            return f.read()
