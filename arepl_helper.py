621149502a161cd5551b1da229592ff631d1d28b
knockjoy <87211271+Knockjoy@users.noreply.github.com>
2023-04-09 09:24:38 +0900

    Update arepl_helper.py

--- arepl_helper.py
+++ arepl_helper.py
@@ -10,11 +10,12 @@ class HelperError(Exception):
     pass
 
 class Input_reader:
-    def __init__(self,path:str="input.txt"):
+    def __init__(self,path:str="input.txt",encoding:str="utf-8"):
         frame = inspect.stack()[1]
         filename = frame[1]
         self.excution_path=Path(filename)
         input_path=Path(path)
+        self.encoding=encoding
         # self.data = self.reader(path).splitlines()
         self.ans_chacker = False
         self.count=0
@@ -28,17 +29,17 @@ class Input_reader:
         frame = inspect.stack()[1]
         filename = frame[1]
         self.file_data = self.reader(filename)
-        const = re.search(r'.*(?=\s*=\s*Input_reader\(\)\s*)',
+        const = re.search(r'.*(?=\s*=\s*.*Input_reader\(.*\)\s*)',
                           self.file_data).group().strip()#変数定義を検索
         self.file_data = re.sub(
             r'from arepl_helper import \*', '', self.file_data)#import文を空白にする
-        self.file_data = re.sub(r'.*Input_reader\(\)', '', self.file_data)#変数定義を空白にする
+        self.file_data = re.sub(r'.*Input_reader\(.*\)', '', self.file_data)#変数定義を空白にする
         self.file_data = re.sub(
             rf'{const}.input\(\)', 'input()', self.file_data)#Input()を組み込み関数のinputにセットする
         self.file_data = re.sub(rf'{const}.compile\(\)', '', self.file_data)#compile()を削除
         self.file_data.strip()
         output_file=Path("output.py")
-        with open(self.excution_path.parent/output_file, "w") as f:
+        with open(self.excution_path.parent/output_file, "w",encoding=self.encoding) as f:
             f.truncate(0)
             f.write(self.file_data)
 
@@ -51,9 +52,8 @@ class Input_reader:
             return res
         else :raise HelperError("inputする要素がありません。")
 
-
     def reader(self, path):
         if not Path(path).exists():
             HelperError("ファイルが見つかりません。")
-        with open(path, "r") as f:
+        with open(path, "r",encoding=self.encoding) as f:
             return f.read()
